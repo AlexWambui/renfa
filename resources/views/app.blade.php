18532d2cc7c2f0f3500f49241ce1289578941093
alexwambui <alexwambuik@gmail.com>
2026-08-17 12:54:27 +0300

    added a proper renfa logo instead of the laravel one.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -30,9 +30,9 @@
             }
         </style>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="icon" href="/assets/images/general/renfa-logo.svg" sizes="any">
+        <link rel="icon" href="/assets/images/general/renfa-logo.svg" type="image/svg+xml">
+        <link rel="apple-touch-icon" href="/assets/images/general/renfa-logo.svg">
 
         @fonts
 
